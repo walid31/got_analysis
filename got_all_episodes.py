4dc7ffbf677241f53ee1b17c7a12a6974c1959b3
Walid31 <ghalemwalid31@gmail.com>
2020-07-09 12:20:36 +0100

    Merging the dataframe

--- got_all_episodes.py
+++ got_all_episodes.py
@@ -12,10 +12,30 @@ def line_prepender(filename, line):
         f.seek(0, 0)
         f.write(line.rstrip('\r\n') + '\n' + content)
 
+
+# # Create a list of sorted seasons
+# seasons_ = os.listdir('Data')
+# seasons = []
+# for season in seasons_:
+#     if season.startswith("season"):
+#         seasons.append(season)
+#         season_path = os.path.join(season)
+#         ds = 'Data/'+season_path
+#         episodes = []
+#         for episode in os.listdir(ds):
+#             if episode.endswith(".txt"):
+#                 episodes.append(episode)
+#         episodes.sort()
+#         print(episodes)
+#         # print (seasons[season])
+# seasons.sort()
+# print(seasons)
+
 for season in os.listdir('Data'):
     if season.startswith("season"):
         season_path = os.path.join(season)
         ds = "Data/"+season_path
+
         for episode in os.listdir(ds):
             if episode.endswith(".txt"):
                 episode_path = os.path.join(episode)
@@ -32,17 +52,16 @@ for season in os.listdir('Data'):
                 counter = counter+1
                 df['season'] = season_path
                 appended_data.append(df)
-                # print(len(appended_data))
+                # print(appended_data)
                 continue
             else:
                 continue
-        continue
-    else:
-        continue
+    continue
 
 df = pd.concat(appended_data)
+# df.sort_values(by=['season'], axis = 0, inplace = True)
 
-# print(df)
+
 
 # ----------- Fist DataFrame formatting----------------
 
@@ -61,10 +80,22 @@ df1 = pd.read_csv('characters.csv')
 df1.drop(df1.columns[0],axis = 1, inplace = True)
 
 df1['name'] = df1['name'].str.lower()
-# df1['name'] = df1['name'].str.replace(r'^(\s*(?:\S+\s+){1})\S+',r'\1') # Delete last name
+df1['name'] = df1['name'].str.replace(r'^(\s*(?:\S+\s+){1})\S+',r'\1') # Delete last name
 df1['name'] = df1['name'].str.strip() # Delete preceding space
 df1.loc[(df1.name == 'khal'),'name']='khal drogo'
 
-print(df1)
-print(df1.name.unique())
+# print(df1)
+# print(df1.name.unique())
 
+# ---------- Merge DataFrames------------------------
+
+merged = df1.merge(df, left_on = 'name', right_on = 'person')
+merged['words'] = [len(x.split()) for x in merged['line'].tolist()]
+
+merged['season'] = merged['season'].map(lambda x: x.lstrip('season').rstrip('aAbBcC'))
+merged['episode'] = merged['episode'].str.replace(r'.txt', '')
+merged['episode'] = merged['episode'].map(lambda x: x.lstrip('e').rstrip('aAbBcC'))
+
+print(merged.head())
+print(df)
+merged.to_csv('Data/all_episodes.csv')
