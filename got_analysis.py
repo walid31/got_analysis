f8401fde79c4ce5fcdb1a2f72d672ecbd77834a6
Walid31 <ghalemwalid31@gmail.com>
2020-07-09 18:13:45 +0100

    per season stacked bar chart

--- got_analysis.py
+++ got_analysis.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from tabulate import tabulate
 import matplotlib.pyplot as plt
-# import plotly.express as px
-# from plotly.subplots import make_subplots
+import plotly.express as px
+from plotly.subplots import make_subplots
 import matplotlib
 from matplotlib.font_manager import FontProperties
 import squarify  
@@ -32,6 +32,22 @@ x2 = x2.tolist()
 x1 = x1.tolist()
 x3 = x3.tolist()
 
-squarify.plot(sizes=x2, label=x1, color =x3,alpha=.7,bar_kwargs=dict(linewidth=1, edgecolor="#000033") )
-plt.axis('off')
-plt.show()
+# squarify.plot(sizes=x2, label=x1, color =x3,alpha=.7,bar_kwargs=dict(linewidth=1, edgecolor="#000033") )
+# plt.axis('off')
+# plt.show()
+
+# ----------------- per season stacked bar chart ---------------
+df3 = df.groupby(['season', 'name', 'sex'])['words'].sum().reset_index(name = 'words_qt')
+fig = px.bar(df3,
+             x = 'season',
+             y = 'words_qt',
+             color = 'sex',
+             barmode = 'stack')
+
+fig.show()
+
+# --------------- per name wordcloud -----------------------
+df4 = df[df['name'] == 'arya']
+
+word = ''
+
